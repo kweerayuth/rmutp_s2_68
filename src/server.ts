35e222abf5bcbb08251a02dd1cd193c96521bff7
Weerayuth Khunrattanasiri <weerayuth.kh@gmail.com>
2025-08-31 15:26:19 +0700

    add more script with dev

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import { serve } from '@hono/node-server';
 import app from './app';
-
 serve(app, (info) => {
     console.log(`Running server on port ${info.port}`);
     //console.log("Running server on port",info.port);
